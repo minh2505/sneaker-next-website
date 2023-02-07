011a8569076fbe0e77c2a9bd931a1c866eb991a1
Minh Co <minh.co@terralogic.com>
2023-02-07 11:28:33 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+module.exports = {
+  trailingSlash: true,
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./src/components/**/*.js", "./pages/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [require("tailwindcss"), require("precss"), require("autoprefixer")],
+};
